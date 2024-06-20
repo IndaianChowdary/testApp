f213c42f2ad2423e722e75b0d6df50e59a85dc28
CHOWDARY <chowdaryk1947@gmail.com>
2024-06-20 15:04:44 +0530

    added mirage image

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import './App.css'
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>SU-30MKI</h1>
+      <h1>MIRAGE 2000</h1>
+      <img src="mirage/mirage.jpg" alt="Mirage 2000"></img>
     </div>
   );
 }
